daa8fe7834162ba02eb156252d6e2ca31b7ad408
rwiuff <rwiuff@gmail.com>
2017-09-28 12:09:29 +0200

    Updated for variable sheet size

--- VNL/NanoSheetCreator.py
+++ VNL/NanoSheetCreator.py
@@ -1,8 +1,18 @@
+# ==============================================================
+# 
+# Script for creating a 2 dimensional nanosheet with predefined
+# hexagonal holes and sheet size using ATKPython.
+#
+# ==============================================================
+
+nC = input("Repetitions along the C axis: ")
+nB = input("Repetitions along the B axis: ")
+
 sheet = NanoSheet(2,2,Carbon)
 
 nlprint(sheet)
-sheet = sheet.repeat(1,5,20)
+sheet = sheet.repeat(1,nB,nC)
 sheet = sheet.center()
 nlprint(sheet)
 
-#nlsave('test',sheet)
+nlsave('test',sheet)
