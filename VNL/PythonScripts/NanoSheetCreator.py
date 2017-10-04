83b926198a79024358aed9af27a8181821a29e67
rwiuff <rwiuff@gmail.com>
2017-10-04 11:22:21 +0200

    Updated Project files

--- VNL/PythonScripts/NanoSheetCreator.py
+++ VNL/PythonScripts/NanoSheetCreator.py
@@ -16,43 +16,42 @@ import math
 nC = input("Repetitions along the C axis: ")
 nB = input("Repetitions along the B axis: ")
 
-sheet = NanoSheet(2,2,Carbon)
+sheet = NanoSheet(2, 2, Carbon)
 
-sheet = sheet.repeat(1,nB,nC)
+sheet = sheet.repeat(1, nB, nC)
 sheet = sheet.center()
 sheetcoor = sheet.cartesianCoordinates()
-sheetcoor = sheetcoor/Ang
+sheetcoordinate = sheetcoor / Ang
 
-str = "The sheet is {} by {} Angstrom".format(sheetcoor[-1,-1],sheetcoor[-1,-2])
+str = "The sheet is {} by {} Angstrom".format(sheetcoor[-1, -1], sheetcoor[-1, -2])
 
 print(str)
 sheetcoor = numpy.array(sheetcoor)
-sheetcoor = numpy.delete(sheetcoor,0,1)
+sheetcoor = numpy.delete(sheetcoor, 0, 1)
 
 # Construct Polygon
 cX = input("Polygon center X-coordinate: ")
 cY = input("Polygon center Y-coordinate: ")
 d = input("Polygon diameter: ")
-r = d/2
+r = d / 2
 p1 = (cX + r, cY)
-p2 = (cX + math.cos(math.pi/3)*r,cY + math.sin(math.pi/3)*r)
-p3 = (cX - math.cos(math.pi/3)*r,cY + math.sin(math.pi/3)*r)
+p2 = (cX + math.cos(math.pi / 3) * r, cY + math.sin(math.pi / 3) * r)
+p3 = (cX - math.cos(math.pi / 3) * r, cY + math.sin(math.pi / 3) * r)
 p4 = (cX - r, cY)
-p5 = (cX - math.cos(math.pi/3)*r,cY - math.sin(math.pi/3)*r)
-p6 = (cX + math.cos(math.pi/3)*r,cY - math.sin(math.pi/3)*r)
-p = path.Path([p1,p2,p3,p4,p5,p6])
+p5 = (cX - math.cos(math.pi / 3) * r, cY - math.sin(math.pi / 3) * r)
+p6 = (cX + math.cos(math.pi / 3) * r, cY - math.sin(math.pi / 3) * r)
+p = path.Path([p1, p2, p3, p4, p5, p6])
 
 # Find atoms inside polygon
 Test = p.contains_points(sheetcoor)
 atoms = numpy.array([])
 for i in range(Test.size):
-	if Test[i] == True:
-		atoms = numpy.append([atoms],[i])
+    if Test[i] == True:
+        atoms = numpy.append([atoms], [i])
 
 # Tag the found atoms
 atoms = atoms.astype(int)
 atoms = atoms.tolist()
-
 
 bulk_configuration = sheet
 
@@ -60,4 +59,4 @@ bulk_configuration.addTags('Hole', atoms)
 
 savename = raw_input("Input filename: ")
 
-nlsave(savename,sheet)
+nlsave(savename, sheet)
