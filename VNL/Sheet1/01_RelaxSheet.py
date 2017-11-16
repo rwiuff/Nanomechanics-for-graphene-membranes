53759b0cf5d3efdd870de449d3dc7021cf18ed1a
rwiuff <rwiuff@gmail.com>
2017-11-16 16:00:05 +0100

    Test project

---
+++ VNL/Sheet1/01_RelaxSheet.py
@@ -0,0 +1,56 @@
+from NanoLanguage import *
+# from NL.CommonConcepts.Configurations.Utilities import cartesian2fractional
+# from NL.CommonConcepts.Configurations.Utilities import fractional2cartesian
+# configuration = nlread('silicene_relax_hexagonal.nc', BulkConfiguration)[-1];
+
+# -------------------------------------------------------------
+# Load File
+# -------------------------------------------------------------
+path = 'Sheet1Fixed.hdf5'
+bulk_configuration = nlread(path, BulkConfiguration)[-1]
+
+# -------------------------------------------------------------
+# Calculator
+# -------------------------------------------------------------
+
+potentialSet = Tersoff_C_2010()
+calculator = TremoloXCalculator(parameters=potentialSet)
+calculator.setVerletListsDelta(0.25 * Angstrom)
+
+bulk_configuration.setCalculator(calculator)
+nlprint(bulk_configuration)
+bulk_configuration.update()
+
+# -------------------------------------------------------------
+# Optimize Geometry
+# -------------------------------------------------------------
+bulk_configuration = OptimizeGeometry(
+    bulk_configuration,
+    max_forces=0.001 * eV / Ang,
+    max_stress=0.001 * eV / Ang**3,
+    max_steps=200,
+    max_step_length=0.2 * Ang,
+    trajectory_filename=None,
+    optimizer_method=LBFGS(),
+    constrain_bravais_lattice=True,
+)
+nlsave('Sheet1Relaxed.hdf5', bulk_configuration)
+nlprint(bulk_configuration)
+
+# -------------------------------------------------------------
+# Optimize Geometry
+# -------------------------------------------------------------
+myConstraints = bulk_configuration.indicesFromTags('Substrate')
+bulk_configuration = OptimizeGeometry(
+    bulk_configuration,
+    max_forces=0.001 * eV / Ang,
+    max_stress=0.001 * eV / Ang**3,
+    max_steps=200,
+    max_step_length=0.2 * Ang,
+    constraints=myConstraints,
+    trajectory_filename=None,
+    optimizer_method=LBFGS(),
+    constrain_bravais_lattice=True,
+)
+nlsave('Sheet1Relaxed.hdf5', bulk_configuration)
+nlprint(bulk_configuration)
