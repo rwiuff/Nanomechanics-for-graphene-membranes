f43f810b895aa175cc1afb0ce34ef6271abb5b03
rwiuff <rwiuff@gmail.com>
2017-11-17 12:59:09 +0100

    Created a smaller system for eigensystem debugging

--- VNL/QuantumWise/phononEigensystem.py
+++ VNL/QuantumWise/phononEigensystem.py
@@ -1,7 +1,6 @@
 # --------------------------------------------------------------------------- #
 # Load libraries
 from NanoLanguage import *
-from pylab import *
 # --------------------------------------------------------------------------- #
 
 # --------------------------------------------------------------------------- #
@@ -10,16 +9,16 @@ myfile = 'DynamicalMatrix.hdf5'
 # --------------------------------------------------------------------------- #
 
 # --------------------------------------------------------------------------- #
-# Load configuration with calculator
+# Load configuration
 configuration = nlread(myfile, BulkConfiguration)[-1]
 # Load DynamicalMatrix
 dynamical_matrix = nlread(myfile, DynamicalMatrix)[-1]
 # --------------------------------------------------------------------------- #
 
 # --------------------------------------------------------------------------- #
-# Calculate eigevalues and eigenvectors from dynamical matrix.
-eigenvalues, eigenvectors = dynamical_matrix.phononEigensystem([0,0,0])
+# Calculate eigevalues and eigenvectors from dynamical matrix at first q-point
+eigenvalues, eigenvectors = dynamical_matrix.phononEigensystem([0, 0, 0])
 # --------------------------------------------------------------------------- #
 
-print(eigenvalues.shape)
-print(eigenvectors.shape)
+print("Number of eigenvalues: {}".format(eigenvalues.shape))
+print("Shape of eigenvector matrix: {}".format(eigenvectors.shape))
